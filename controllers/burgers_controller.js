5461d094fe94f1c0fb03e8a594e6798fdd82390f
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 17:22:33 -0600

    debugging

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,7 @@ var burger = require("../models/burger.js");
 //Create all our routes and set up logic within those routes where required.
 
 router.get('/', function (req, res){
-    burger.selectall(function (data){
+    burger.selectAll(function (data){
         var hbsObject = { burgers: data};
         console.log(hbsObject);
         res.render("index", hbsObject);

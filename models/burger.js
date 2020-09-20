05e3ca3830a025561454a3870af06f125add7d5d
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 12:50:23 -0600

    code to call the ORM functions with specific burger inputs

--- models/burger.js
+++ models/burger.js
@@ -2,12 +2,33 @@
 
 var orm = require("..config/orm.js");
 
+var burger = {
+    all: function(cb){
+        orm.all( 'burgers', function(res){
+            cb(res);
+        });
+    },
 
+    // The variables cols and vals are arrays.
 
+    createOne: function(bruger_name, cb) {
+        orm.createOne('burgers', 'burger_name', function(res) {
+            cb(res);
+        });
+    },
 
+    updateOne: function(burger_id,cb){
+        orm.updateOne('burgers', 'devoured', true, burger_id, function (res){
+            cb(res);
+        });
+    },
 
-
-
+    restoreOne: function(burger_id, cb) {
+        orm.updateOne('burgers', 'devoured', false, burger_id, function(res){
+            cb(res)
+        });
+    }
+};
 
 // Export the database functions for the controller (catsController.js).
 module.exports = burger;

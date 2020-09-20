01145fa7b27ddc1fe668c3dfe94ad4641f6e31f9
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 12:23:02 -0600

    Update to config files and db files

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,13 @@
 ### Schema
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
+
 USE burgers_db;
 
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN default false,
     PRIMARY KEY (id)
 );
 

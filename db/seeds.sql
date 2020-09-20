ccac165636a308892d46a25ff891c8af14eb2919
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 10:53:40 -0600

    Added 3 entries in seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Nacho Burger', FALSE);
+INSERT INTO burger (burger_name, devoured) VALUES ('Western BBQ Burger', FALSE);
+INSERT INTO burger (burger_name, devoured) VALUES ('Heart Attack Double Bacon Burger');

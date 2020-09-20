de3e6e56c8a9f09e9efe5d3bfa5d5c5aa93762ec
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 11:38:44 -0600

    Fixed/Update seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Nacho Burger', FALSE);
-INSERT INTO burger (burger_name, devoured) VALUES ('Western BBQ Burger', FALSE);
-INSERT INTO burger (burger_name, devoured) VALUES ('Heart Attack Double Bacon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western BBQ Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Heart Attack Double Bacon Burger');

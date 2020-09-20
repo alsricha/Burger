2e84e205255632cfb0e7ccdfff545327ccb39eb2
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 10:45:45 -0600

    schema file update

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
-### SCHEMA
+### Schema
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
-)
+);
+

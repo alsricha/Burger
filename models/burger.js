eea3436e105bf2324404158e49c6078bebe3ae51
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 17:51:03 -0600

    corrected syntax

--- models/burger.js
+++ models/burger.js
@@ -1,34 +1,25 @@
 // Import the ORM to create functions that will interact with the database.
 
-var orm = require("..config/orm.js");
+var orm = require("../config/orm.js");
 
-var burger = {
-    selectAll: function(cb){
-        orm.selectAll( 'burgers', function(res){
-            cb(res);
-        });
-    },
-
-    // The variables cols and vals are arrays.
-
-    createOne: function(bruger_name, cb) {
-        orm.createOne('burgers', 'burger_name', function(res) {
-            cb(res);
-        });
-    },
-
-    updateOne: function(burger_id,cb){
-        orm.updateOne('burgers', 'devoured', true, burger_id, function (res){
-            cb(res);
-        });
-    },
-
-    restoreOne: function(burger_id, cb) {
-        orm.updateOne('burgers', 'devoured', false, burger_id, function(res){
-            cb(res)
-        });
-    }
+var burgers = {
+  SelectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  createOne: function(cols, vals, cb) {
+    orm.createOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
-// Export the database functions for the controller (catsController.js).
-module.exports = burger;
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burgers;

a6895245ebdb68781bfc79c24a9b9beddf46134a
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 13:16:25 -0600

    devour burger route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,12 +16,18 @@ router.get('/', function (req, res){
 });
 
 //Create a New Burger to the database
-router.post('/burger/create', function (req, res) {
+router.post("/burger/create", function (req, res) {
     burger.CreateOne(req.body.burger_name, function() {
-      res.redirect('/');
+      res.redirect("/");
     });
   });
 
+  //Devour a Burger
+  router.post("/burger/eat", function (req, res){
+      burger.updateOne(req.body.id, function(){
+          res.redirect("/");
+      });
+  });
 
 
 // Export routes for server.js to use

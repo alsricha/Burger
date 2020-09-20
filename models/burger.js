f4267487860883b9f2e5a706acc8e68ab6935b16
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 17:24:42 -0600

    debugging

--- models/burger.js
+++ models/burger.js
@@ -3,8 +3,8 @@
 var orm = require("..config/orm.js");
 
 var burger = {
-    all: function(cb){
-        orm.all( 'burgers', function(res){
+    selectAll: function(cb){
+        orm.selectAll( 'burgers', function(res){
             cb(res);
         });
     },

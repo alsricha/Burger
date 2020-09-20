55e2169f81f13666488c8733856a9eb820445d4a
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 17:06:53 -0600

    Revert to original code

--- config/connection.js
+++ config/connection.js
@@ -15,14 +15,14 @@ connection = mysql.createConnection(process.env.JAWSDB_URL);
 	});
 };
 
-/* connection.connect(function (err) {
+connection.connect(function (err) {
 	if (err) {
 		console.error('error connecting: ' + err.stack);
 		return;
 	}
 	console.log('connected as id ' + connection.threadId);
 });
- */
-connection.connect();
+ 
+
 // Export connection for our ORM to use.
 module.exports = connection;

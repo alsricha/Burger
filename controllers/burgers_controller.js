001fbd239fad2c228828761bc8b993b74faa1b6a
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 17:44:30 -0600

    reviewd file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,39 +2,54 @@ var express = require("express");
 
 var router = express.Router();
 
-//Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+// Import the model (cat.js) to use its database functions.
+var burgers = require("../models/burgers.js");
 
-//Create all our routes and set up logic within those routes where required.
-
-router.get('/', function (req, res){
-    burger.selectAll(function (data){
-        var hbsObject = { burgers: data};
-        console.log(hbsObject);
-        res.render("index", hbsObject);
-    });
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burgers.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-//Create a New Burger to the database
-router.post("/burger/create", function (req, res) {
-    burger.CreateOne(req.body.burger_name, function() {
-      res.redirect("/");
-    });
+router.post("/api/burgers", function(req, res) {
+  console.log(res.body)
+  burgers.create([
+    "name", "eaten"
+  ], [
+    req.body.name, req.body.eaten
+  ], function(result) {
+    console.log(result)
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
   });
+});
 
-//Devour a Burger
-router.post("/burger/eat", function (req, res){
-    burger.updateOne(req.body.id, function(){
-        res.redirect("/");
-    });
-  });
-  router.post("/burger/restore", function (req, res){
-      burger.restoreOne(reg.body.id, function(){
-          res.redirect("/");
-      });
-  });
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burgers.update(
+    {
+      eaten: req.body.eaten
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
 
 
-// Export routes for server.js to use
-
+// Export routes for server.js to use.
 module.exports = router;

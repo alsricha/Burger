cb2eb957b81e26a491082b33a119a8be8a717b8a
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-20 19:29:43 -0600

    added config.json

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+/* var db = require("./models"); */
 
 var port = process.env.PORT || 3306;
 
 var app = express();
+
+/* db.sequelize.sync().then(function(){
+  app.listen(PORT,function(){
+    console.log("Listening on port %s", PORT);
+  });
+}); */
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
